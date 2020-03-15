c5a45a97693837e51147d8e1d665497335eee1c4
Daniel <danielgg0831@gmail.com>
2020-03-15 18:42:10 -0300

    Implemented the basic style for the app and the urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Product, Order, Customer
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_orders = orders.count()
+    total_customers = customers.count()
+    total_delivered = orders.filter(status='Delivered').count()
+    total_pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers, 'total_orders': total_orders,
+               'total_customers': total_customers, 'total_delivered': total_delivered, 'total_pending': total_pending}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, customer_id):
+    customer = Customer.objects.get(id=customer_id)
+    orders = customer.order_set.all()
+    orders_count = orders.count()
+    context = {'customer': customer, 'orders': orders,
+               'orders_count': orders_count}
+    return render(request, 'accounts/customer.html', context)
